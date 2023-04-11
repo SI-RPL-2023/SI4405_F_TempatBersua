df41e77d363998540ae7ac52387a9bcd86d656be
TriRetnoPrayudati <triretnoprayudati@student.telkomuniversity.ac.id>
2023-04-11 22:23:01 +0700

    add login logout admin #9

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,7 +15,6 @@ use App\Http\Controllers\UserController;
 |
 */
 
-
 Route::get('/', function () {
     return view('homepage');
 });
@@ -24,3 +24,10 @@ Route::post('/register',[UserController::class,'store']);
 Route::get('/login',[UserController::class,'loginPage']);
 Route::post('/login',[UserController::class,'login']);
 Route::put('/logout/{id}', [UserController::class, 'logout']);
+
+// Admin
+Route::get('/admin', function () {
+    $user = User::where('status', 'logged in')->get();
+    return view('adminPage',compact('user'));
+});
+
